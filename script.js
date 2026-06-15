26a6bee003925d5bfc196eb92166cc0e01c73af2
Grace <gracehui30@gmail.com>
2026-06-15 19:31:09 -0400

    updates portfolio

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-// Add JavaScript code for your web site here and call it from index.html.
-const resume_button = document.querySelector('#resume-button');
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    // Check if the element is currently visible in the viewport
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+    else{
+      entry.target.classList.remove('show');
+    }
+  });
+};
 
-const successMessage = () => {
-    console.log("You have successfully downloaded my resume!");
-}
+// shows when 30% of the element is visible
+const observerOptions = {
+  root: null, // Defaults to the browser viewport
+  threshold: 0.2
+};
 
-resume_button.addEventListener('click', successMessage);
+// create the observer instance
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+// select and observe all targeted elements
+const elements = document.querySelectorAll('.projects-scroll');
+elements.forEach(el => observer.observe(el));
+
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+  link.addEventListener('click', function() {
+    links.forEach(item => item.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
